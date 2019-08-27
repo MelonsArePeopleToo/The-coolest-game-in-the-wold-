e4d929739ad9dc3b1d471eb3b28fd33d41bd1a43
Alex Dynkov <aldynkov@gmail.com>
2019-08-28 02:36:14 +0300

    stArt mAp

--- GaAaME/game.cpp
+++ GaAaME/game.cpp
@@ -1,5 +1,7 @@
 #include "game.h"
 #include "ui_game.h"
+#include <QMovie>
+#include <QPixmap>
 
 Game::Game(QWidget *parent) :
     QMainWindow(parent),
@@ -7,12 +9,19 @@ Game::Game(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    QPixmap pixmap(":/img/PANDA.png"); //будет карта
+    QSize PicSize(1000,1000);
+    pixmap = pixmap.scaled(PicSize, Qt::KeepAspectRatio);
+
     scene = new QGraphicsScene();
+    scene->addPixmap(pixmap);
+
 
     ui->graphicsView->setScene(scene);
     scene->setSceneRect(-2500,-2500,5000,5000);
     ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
 
     scene->addLine(-2500,0,2500,0,QPen(Qt::black));
     scene->addLine(0,-2500,0,2500,QPen(Qt::black));
